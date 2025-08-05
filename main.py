850625c915424a5be5325911ca2d407ab72530f5
Suhaas15 <suhaas15032001@gmail.com>
2025-08-05 16:21:54 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+
+GENDER = "male"
+WEIGHT_KG = "177lbs"
+HEIGHT_CM = "6'3"
+AGE = "24"
+
+APP_ID = YOUR APP ID
+API_KEY = YOUR API KEY
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
